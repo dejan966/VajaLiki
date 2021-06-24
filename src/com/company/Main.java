cb3762723a4da372dc5687f7168e56fa8026ec4b
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-06-24 18:48:32 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Vpišite širino: ");
+        int s = in.nextInt();
+        System.out.println(s);
+
+
+    }
+}
