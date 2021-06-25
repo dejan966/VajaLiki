98ad995032b47b6e6f2f895936cd83389e445838
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-06-25 20:37:30 +0200

    Prva polovica narjena:    *   * *  *   * *     *

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,17 +12,11 @@ public class Main {
         System.out.println(v);
 
         int st = 1;
-
         int Stv=v;
         int Sts=v;
-        while(Stv!=0){
-            //Sts-v (kok stolpcev je se ostalo do presledka)
-            //int OP = Sts-v;
-            int OP=0;
-            //int Stz=0;
 
-            //int g = v- st;
-            //int p = Stv-1;
+        while(Stv!=0){
+            int OP=2;
             int Stp=1;
 
             while(Stp<Stv){
@@ -30,16 +24,8 @@ public class Main {
                 Stp++;
             }
 
-            if(Stp<=v) //ali Stp<=Sts
+            if(Stp<=Sts) //ali Stp<=v
                 System.out.print("*");
-
-            /*while(OP>0){
-                System.out.print(" ");
-                OP--;
-            }
-
-            if(OP<=0)
-                System.out.print("*");*/
 
             if(Stp!=v){
                 if(st%2!=0){
@@ -50,52 +36,6 @@ public class Main {
                 }
                 System.out.print("*");
             }
-
-            /*while(Stz<Sts){
-                System.out.print("*");
-                //Sts++;
-                Stz++;
-            }*/
-
-            /*if(p>=0){
-                System.out.print(" ");
-                Sts++;
-                p--;
-                if(Sts==4)
-                    System.out.println();
-            }
-            if(Stz<=Stv){
-                System.out.print("*");
-                Sts++;
-                Stz++;
-                if(Sts==4)
-                    System.out.println();
-            }*/
-
-
-            /*if(s%2!=0){
-                while(p!=0){
-                    System.out.print(" ");
-                    p--;
-                }
-                if(Stv==1){
-                    Stv++;
-                    System.out.print("*");
-                }
-                else{
-                    //nared da izpiše a koncu še eno zvezdico pa pol še da izpiše še desno stran
-                    while(Stz!=Stv){
-                        System.out.print("*");
-                        Stz++;
-                    }
-                }
-                System.out.println();
-            }*/
-
-
-            //Stv+=2;
-            //v--;
-
 
             System.out.println();
 
