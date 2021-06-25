7694258bb737253cef3beab9e78872f662931d6c
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-06-25 18:26:09 +0200

    izpisana leva stran zvezdic(širina 4):    *   *  * *

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,50 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Vpišite širino: ");
-        int sirina = in.nextInt();
-        System.out.println(sirina);
+        int v = in.nextInt();
+        System.out.println(v);
 
-        int s = 0;
-        int Stv=1;
+        int st = 0;
 
-        while(s<=sirina){
+        int Stv=v;
+        int Sts=v;
+        while(Stv!=0){
             int Stz=0;
-            int g = sirina- s;
-            int p = g/2+1;
 
-            if(s%2!=0){
+            //int g = v- st;
+            int p = Stv-1;
+            int Stp=1;
+
+            //nek if-else da sproti preverja stanje zvezdic pa presledkov
+            while(Stp<Stv){
+                System.out.print(" ");
+                Stp++;
+            }
+
+            //brez while je to sam leva stran zvezdic
+            if(Stp<=Sts)
+                System.out.print("*");
+
+
+
+            System.out.println();
+            /*if(p>=0){
+                System.out.print(" ");
+                Sts++;
+                p--;
+                if(Sts==4)
+                    System.out.println();
+            }
+            if(Stz<=Stv){
+                System.out.print("*");
+                Sts++;
+                Stz++;
+                if(Sts==4)
+                    System.out.println();
+            }*/
+
+
+            /*if(s%2!=0){
                 while(p!=0){
                     System.out.print(" ");
                     p--;
@@ -36,12 +68,13 @@ public class Main {
                     }
                 }
                 System.out.println();
-            }
+            }*/
 
 
             //Stv+=2;
-            s++;
-
+            //v--;
+            Stv--;
+            Sts++;
         }
     }
 }
